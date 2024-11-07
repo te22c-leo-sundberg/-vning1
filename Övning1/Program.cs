297a70cf805b65755244fb8d3168e066a8c90446
Leo Sundberg <leo.sundberg@elev.ga.ntig.se>
2024-11-07 10:37:33 +0100

    first commit

---
+++ Övning1/Program.cs
@@ -0,0 +1,12 @@
+﻿string username = "";
+string password = "";
+
+
+while(username != "noname" || password != "nopass")
+{
+    Console.WriteLine("Write your username and password to procceed.");
+    username = Console.ReadLine();
+    password = Console.ReadLine();
+}
+Console.WriteLine("Welcome");
+Console.ReadLine();
